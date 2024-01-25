d9f15464439a210d7bd4f7064440241fb70c8a7b
MarceliEDU <marcelihryniuk.edu@gmail.com>
2024-01-25 23:50:29 +0100

    zmiana layoutu dla logowania i rejestrowania

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,17 @@
-@extends('layouts.app')
+@extends('layout')
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center mt-5">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">
+                    {{ __('Login') }}
+
+                    @if (Route::has('register'))
+                    <a class="float-right" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    @endif
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,16 @@
-@extends('layouts.app')
+@extends('layout')
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center mt-5">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">{{ __('Register') }}
+
+                @if (Route::has('login'))
+                    <a class="float-right" href="{{ route('login') }}">{{ __('Login') }}</a>
+                @endif
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
